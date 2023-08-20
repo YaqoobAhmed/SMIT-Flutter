53bc951525a2c26f002fcbce89e2a272bdd5c41d
YaqoobAhmed <yaqoobahmed29@gmail.com>
2023-08-20 21:22:18 +0500

    Making a new screen file

--- state_full_practice/lib/main.dart
+++ state_full_practice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'state_full_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -64,52 +65,3 @@ class MyApp extends StatelessWidget {
 //       ),
 //     );
 //   }
-// }
-
-class Screen extends StatefulWidget {
-  const Screen({super.key});
-  @override
-  State<Screen> createState() => _ScreenState();
-}
-
-class _ScreenState extends State<Screen> {
-  List<String> nameList = [];
-  TextEditingController addname = TextEditingController();
-
-  void newEntry() {
-    setState(() {
-      nameList.add(addname.text);
-      addname.clear();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: addname,
-          decoration: const InputDecoration(
-            hintText: 'Enter your Name',
-          ),
-        ),
-        actions: [
-          ElevatedButton(
-              onPressed: () {
-                newEntry();
-              },
-              child: const Text("Add"))
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: nameList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            tileColor: Colors.green[100],
-            title: Text(nameList[index]),
-          );
-        },
-      ),
-    );
-  }
-}
