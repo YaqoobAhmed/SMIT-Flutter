92aef9336e5ac2b47a350b1edd60f97c62e460c2
YaqoobAhmed <yaqoobahmed29@gmail.com>
2023-08-30 18:16:24 +0500

    Created Animated Splash Screen

---
+++ mini_project_1/lib/Screens/Splash/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mini_project_1/Screens/Onboarding/Screens/onboarding_screen1.dart';
+import 'package:mini_project_1/Screens/Onboarding/Widgets/onboard_heading_text.dart';
+import 'package:mini_project_1/Screens/Widgets/colors.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Onboarding1(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AllColors.primarycolor,
+      body: Center(
+        child: Container(
+          child: AnimatedTextKit(animatedTexts: [
+            TyperAnimatedText("Mini Project",
+                textAlign: TextAlign.center,
+                textStyle: TextStyle(
+                  color: AllColors.HeadingTextColor,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Manrope",
+                ),
+                speed: Duration(milliseconds: 120))
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+//OnboardHeadingText(data: "Mini Project")
--- mini_project_1/lib/main.dart
+++ mini_project_1/lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-
-import 'Screens/Onboarding/Screens/onboarding_screen1.dart';
+import 'package:mini_project_1/Screens/Splash/splash_screen.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Mini Project",
-      home: Onboarding1(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
