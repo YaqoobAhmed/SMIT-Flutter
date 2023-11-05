d8ccb4e6d875ac57a2167def6fe98a03db74a1be
Yaqoob Ahmed <98280210+YaqoobAhmed@users.noreply.github.com>
2023-11-05 17:10:37 +0500

    added all the localization data and setup

--- ToDo List With StatefulWidget/lib/main.dart
+++ ToDo List With StatefulWidget/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:state_full_practice/Theme/dark_theme.dart';
 import 'package:state_full_practice/Theme/light_theme.dart';
+import 'package:state_full_practice/l10n/l10n.dart';
 import 'state_full_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +16,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      debugShowCheckedModeBanner: false,
-      home: Screen(),
-    );
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        debugShowCheckedModeBanner: false,
+        home: const Screen(),
+        supportedLocales: L10n.all,
+        locale: const Locale('en'),
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ]);
   }
 }
