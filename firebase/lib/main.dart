566285c44abf2241aab40217342bcb97a67b2b9c
Yaqoob Ahmed <98280210+YaqoobAhmed@users.noreply.github.com>
2023-11-26 18:20:12 +0500

    initializing firebase

---
+++ firebase/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase/Views/Register/register.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: RegisterView(),
+    );
+  }
+}
