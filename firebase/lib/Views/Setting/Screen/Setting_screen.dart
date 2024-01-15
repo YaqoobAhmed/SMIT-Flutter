0fbd48627947249ba47b42fa91bc015e4c7e4452
Yaqoob Ahmed <98280210+YaqoobAhmed@users.noreply.github.com>
2024-01-16 00:58:31 +0500

    done with setting page

--- firebase/lib/Views/Setting/Screen/Setting_screen.dart
+++ firebase/lib/Views/Setting/Screen/Setting_screen.dart
@@ -17,164 +17,177 @@ class SettingScreen extends StatelessWidget {
           ),
           centerTitle: true,
         ),
-        body: SizedBox(
-          width: MediaQuery.of(context).size.width * 1,
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-                child: Stack(children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.1,
-                    width: MediaQuery.of(context).size.width * 1,
-                    decoration: BoxDecoration(
-                        color: blueColor,
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(400, 100),
-                            bottomRight: Radius.elliptical(400, 100))),
-                  ),
-                  Positioned(
-                    top: 35,
-                    left: 150,
-                    child: CircleAvatar(
-                      radius: 45,
-                      child: Center(
-                        child: Icon(
-                          Icons.person,
-                          size: 50,
-                          color: Colors.grey.shade700,
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  opacity: 0.8,
+                  image: AssetImage("assets/images/settings.jpg"),
+                  fit: BoxFit.cover)),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 1,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Stack(children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      width: MediaQuery.of(context).size.width * 1,
+                      decoration: BoxDecoration(
+                          color: blueColor,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.elliptical(400, 100),
+                              bottomRight: Radius.elliptical(400, 100))),
+                    ),
+                    Positioned(
+                      top: 35,
+                      left: 150,
+                      child: CircleAvatar(
+                        radius: 46,
+                        backgroundColor: Colors.black,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey.shade300,
+                          radius: 45,
+                          child: Center(
+                            child: Icon(
+                              Icons.person,
+                              size: 50,
+                              color: blueColor,
+                              // color: Colors.grey.shade700,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                      bottom: 35,
-                      right: 150,
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: blueColor,
-                      )),
-                ]),
-              ),
-              Text(
-                "Hassan Mirza",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              // Wrap(crossAxisAlignment: WrapCrossAlignment.center, children: [
-              //   Text(
-              //     "Name:",
-              //     style: TextStyle(
-              //         fontSize: 20,
-              //         fontWeight: FontWeight.bold,
-              //         color: blueColor),
-              //   ),
-              //   SizedBox(
-              //     width: 10,
-              //   ),
-              //   Text(
-              //     "Yaqoob Ahmed",
-              //     style: TextStyle(
-              //       fontSize: 20,
-              //     ),
-              //   )
-              // ]),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Text(
-                          "Email:",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "hassanmirza@gmail.com",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        )
-                      ]),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Text(
-                          "Contact:",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "03062694259",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        )
-                      ]),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Text(
-                          "NIC No:",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "42101-XXXXXXX-X",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        )
-                      ]),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      FirebaseAuth.instance.signOut();
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginView(),
-                          ));
-                    },
-                    child: Wrap(
+                    Positioned(
+                        bottom: 35,
+                        right: 150,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: blueColor,
+                        )),
+                  ]),
+                ),
+                Text(
+                  "Hassan Mirza",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                // Wrap(crossAxisAlignment: WrapCrossAlignment.center, children: [
+                //   Text(
+                //     "Name:",
+                //     style: TextStyle(
+                //         fontSize: 20,
+                //         fontWeight: FontWeight.bold,
+                //         color: blueColor),
+                //   ),
+                //   SizedBox(
+                //     width: 10,
+                //   ),
+                //   Text(
+                //     "Yaqoob Ahmed",
+                //     style: TextStyle(
+                //       fontSize: 20,
+                //     ),
+                //   )
+                // ]),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    Wrap(
                         crossAxisAlignment: WrapCrossAlignment.center,
                         children: [
                           Text(
-                            "Signout",
+                            "Email:",
                             style: TextStyle(
                                 fontSize: 20, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(
                             width: 10,
                           ),
-                          Icon(Icons.logout)
+                          Text(
+                            "hassanmirza@gmail.com",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )
                         ]),
-                  )
-                ],
-              ),
-            ],
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Text(
+                            "Contact:",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "03062694259",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )
+                        ]),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Text(
+                            "NIC No:",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "42101-XXXXXXX-X",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )
+                        ]),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        FirebaseAuth.instance.signOut();
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginView(),
+                            ));
+                      },
+                      child: Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Text(
+                              "Signout",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(Icons.logout)
+                          ]),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
